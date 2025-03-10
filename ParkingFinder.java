d60e82d296738bcdbcdd537b625dac3f61ac040c
Evan Varozza <VarozzaEJ@gmail.com>
2025-03-10 14:03:50 -0600

    Step Four

--- ParkingFinder.java
+++ ParkingFinder.java
@@ -8,6 +8,7 @@ public class ParkingFinder {
         Scanner kbdInput = new Scanner(System.in);
         long seed;
         int timeNeededToPark, carX, carY, distanceOne, distanceTwo, distanceThree, distanceFour;
+        double priceOne, priceTwo, priceThree, priceFour;
         double random = Math.random() * 99;
         double randomTwo = Math.random() * 99;
         double randomThree = Math.random() * 99;
@@ -42,17 +43,35 @@ public class ParkingFinder {
 
         System.out.println(carX + " " + carY);
         System.out.println("");
-        System.out.println(spotOne);
-        System.out.println(spotTwo);
-        System.out.println(spotThree);
-        System.out.println(spotFour);
-
+        
         distanceOne = (Math.abs((spotOne.getLocationX() - carX)) + Math.abs((spotOne.getLocationY() - carY)));
         distanceTwo = (Math.abs((spotTwo.getLocationX() - carX)) + Math.abs((spotTwo.getLocationY() - carY)));
         distanceThree = (Math.abs((spotThree.getLocationX() - carX)) + Math.abs((spotThree.getLocationY() - carY)));
         distanceFour = (Math.abs((spotFour.getLocationX() - carX)) + Math.abs((spotFour.getLocationY() - carY)));
+        
+        if(timeNeededToPark % 10 != 0) {
+            timeNeededToPark = (timeNeededToPark / 10) * 10 + 10; 
+        }
+        
+        priceOne = ((timeNeededToPark / 10) * spotOne.getCostPerInterval());
+        priceTwo = ((timeNeededToPark / 10) * spotTwo.getCostPerInterval());
+        priceThree = ((timeNeededToPark / 10) * spotThree.getCostPerInterval());
+        priceFour = ((timeNeededToPark / 10) * spotFour.getCostPerInterval());
 
-        System.out.println(distanceOne);
+        System.out.println(" ");
+        System.out.println("Spot One: " + spotOne);
+        System.out.println("    Distance: " + distanceOne);
+        System.out.println("    Total Cost: " + priceOne);
+        System.out.println("Spot Two: " + spotTwo);
+        System.out.println("    Distance: " + distanceTwo);
+        System.out.println("    Total Cost: " + priceTwo);
+        System.out.println("Spot Three: " + spotThree);
+        System.out.println("    Distance: " + distanceThree);
+        System.out.println("    Total Cost: " + priceThree);
+        System.out.println("Spot Four: " + spotFour);
+        System.out.println("    Distance: " + distanceFour);
+        System.out.println("    Total Cost: " + priceFour);
+
         kbdInput.close();
     }
 }
